f4b7afcc3bc4ae6d86acedc1f7ccbb662f946428
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2023-11-02 23:41:06 +0300

    success start OOP encoders!

--- src/arduino_encoder.h
+++ src/arduino_encoder.h
@@ -4,57 +4,96 @@
 
    author: Egor Bakay <egor_bakay@inbox.ru> Ni3nayka
    write:  February 2022
-   modify: April 2023
+   modify: november 2023
 */
 
 #pragma once
 
-//#include "PinChangeInterrupt.h"
-#include "Pins.h"
-#include "EncMotor.h"
-//#include "EncMotor_1.h"
+#include "PinChangeInterrupt.h"
 
-#ifndef ENC_MOTOR_PARROT_CM
-#define ENC_MOTOR_PARROT_CM 157
-#endif
-#ifndef ENC_MOTOR_PARROT_ANGLE
-#define ENC_MOTOR_PARROT_ANGLE 25
-#endif
+// https://habr.com/ru/articles/527044/
+// http://cppstudio.com/post/6964/
 
-#define MAX_ENCODERS_QUANTITY 4 // 10
+#define ENC_NONE -1
+#define MAX_QUANTITY_ENC 8
 
-class Encoders {
+#define ENC_SETUP_INTERRUPT(interrupt_pin,interrupt_fun) attachPCINT(digitalPinToPCINT(interrupt_pin), interrupt_fun, CHANGE); 
+#define ENC_SETUP_IF_FOR_INTERRUPT(enc_number_test,pin_a,pin_b,fun_1,fun_2) if (Encoder::enc_number==enc_number_test) { ENC_SETUP_INTERRUPT(pin_a,fun_1); if (pin_b!=ENC_NONE) ENC_SETUP_INTERRUPT(pin_b,fun_2); }
+
+int enc_counter__NOT_USING = 0;
+bool enc_dat__NOT_USING[MAX_QUANTITY_ENC][5] = {0}; // [enc_number] [a_new,a_last,b_new,b_last,reverse_enc]
+long int enc_count__NOT_USING[MAX_QUANTITY_ENC] = {0};
+
+class Encoder {
   public:
-
-    int add(int pin_1, int pin_2=-1); // return number
-    void reverse(int number);
+    long int enc;
     
-    long int value[MAX_ENCODERS_QUANTITY] = {0};
-    long int read(int number);
-    void clear(int number);
+    void setup(int pin_a, int pin_b=ENC_NONE) {
+      Encoder::enc_number = enc_counter__NOT_USING;
+      enc_counter__NOT_USING++;
+      pinMode(pin_a,INPUT);
+      if (pin_b!=ENC_NONE) 
+        pinMode(pin_b,INPUT);
+      enc_dat__NOT_USING[Encoder::enc_number][0] = digitalRead(pin_a);
+      enc_dat__NOT_USING[Encoder::enc_number][2] = digitalRead(pin_b);
+      enc_dat__NOT_USING[Encoder::enc_number][1] = enc_dat__NOT_USING[Encoder::enc_number][0];
+      enc_dat__NOT_USING[Encoder::enc_number][3] = enc_dat__NOT_USING[Encoder::enc_number][2];
+      enc_dat__NOT_USING[Encoder::enc_number][4] = 0;
+      enc_count__NOT_USING[Encoder::enc_number] = 0;
+      ENC_SETUP_IF_FOR_INTERRUPT(0,pin_a,pin_b,Encoder::interrupt_enc_1,Encoder::interrupt_enc_2);
+      ENC_SETUP_IF_FOR_INTERRUPT(1,pin_a,pin_b,Encoder::interrupt_enc_3,Encoder::interrupt_enc_4);
+      ENC_SETUP_IF_FOR_INTERRUPT(2,pin_a,pin_b,Encoder::interrupt_enc_5,Encoder::interrupt_enc_6);
+      ENC_SETUP_IF_FOR_INTERRUPT(3,pin_a,pin_b,Encoder::interrupt_enc_7,Encoder::interrupt_enc_8);
+      ENC_SETUP_IF_FOR_INTERRUPT(4,pin_a,pin_b,Encoder::interrupt_enc_9,Encoder::interrupt_enc_10);
+      ENC_SETUP_IF_FOR_INTERRUPT(5,pin_a,pin_b,Encoder::interrupt_enc_11,Encoder::interrupt_enc_12);
+      ENC_SETUP_IF_FOR_INTERRUPT(6,pin_a,pin_b,Encoder::interrupt_enc_13,Encoder::interrupt_enc_14);
+      ENC_SETUP_IF_FOR_INTERRUPT(7,pin_a,pin_b,Encoder::interrupt_enc_15,Encoder::interrupt_enc_16);
+    }
+    void reverse() {
+      enc_dat__NOT_USING[Encoder::enc_number][4] = !enc_dat__NOT_USING[Encoder::enc_number][4];
+    }
+  private:
+    int enc_number;
 
-    // =========================================================
-    enc_motor motor_A;
+    /*static void Interrupt_enc_A() {
+      enc_motor::enc_dat_A_real = !enc_motor::enc_dat_A_real;
+      enc_motor::encoder_update();
+    }
+    static void Interrupt_enc_B() {
+      enc_motor::enc_dat_B_real = !enc_motor::enc_dat_B_real;
+      enc_motor::encoder_update();
+    }
+    static void enc_motor::encoder_update() {
+      if ((enc_motor::enc_dat_A_real!=enc_motor::enc_dat_B_last)!=enc_motor::reverse_enc_flag) enc_motor::enc++;
+      else enc_motor::enc--;
+      enc_motor::enc_dat_A_last = enc_motor::enc_dat_A_real;
+      enc_motor::enc_dat_B_last = enc_motor::enc_dat_B_real;
+    }*/
     
-    void setup() {
-      Encoders::motor_A.setup(L298P_PWMA,L298P_DIRA,L298P_ENCA1,L298P_ENCA2);
+    static void interrupt_common(int interrupt_number) {
+      int enc_number = (interrupt_number-1)/2;
+      enc_dat__NOT_USING[enc_number][0] = enc_dat__NOT_USING[enc_number][0]!=(interrupt_number%2);
+      enc_dat__NOT_USING[enc_number][2] = enc_dat__NOT_USING[enc_number][2]==(interrupt_number%2);
+      enc_count__NOT_USING[enc_number] += int((enc_dat__NOT_USING[enc_number][0]!=enc_dat__NOT_USING[enc_number][3])!=enc_dat__NOT_USING[enc_number][4])*2-1;
+      enc_dat__NOT_USING[enc_number][1] = enc_dat__NOT_USING[enc_number][0];
+      enc_dat__NOT_USING[enc_number][3] = enc_dat__NOT_USING[enc_number][2];
+      Serial.println(String(enc_number)+" "+String(enc_count__NOT_USING[enc_number]));
     }
-    void motors(long int speed_a, long int speed_b) {
-      Encoders::motor_A.run(speed_a);
-    }
-    void enc_run_update() {
-      
-    }
-  
-
-//    void reverse_motor_A() { Encoders::motor_A.reverse_motor = Encoders::motor_A.reverse_motor==1?-1:1; }
-//    void reverse_motor_B() { Encoders::motor_B.reverse_motor = Encoders::motor_B.reverse_motor==1?-1:1; }
-//    void reverse_enc_A() { Encoders::motor_A.reverse_enc = !Encoders::motor_A.reverse_enc; }
-//    void reverse_enc_B() { Encoders::motor_B.reverse_enc = !Encoders::motor_B.reverse_enc; }
     
-//  private:
-//    int reverse_MA, reverse_MB; 
-//    bool reverse_EA, reverse_EB; 
+    static void interrupt_enc_1()  { Encoder::interrupt_common(1);  }
+    static void interrupt_enc_2()  { Encoder::interrupt_common(2);  }
+    static void interrupt_enc_3()  { Encoder::interrupt_common(3);  }
+    static void interrupt_enc_4()  { Encoder::interrupt_common(4);  }
+    static void interrupt_enc_5()  { Encoder::interrupt_common(5);  }
+    static void interrupt_enc_6()  { Encoder::interrupt_common(6);  }
+    static void interrupt_enc_7()  { Encoder::interrupt_common(7);  }
+    static void interrupt_enc_8()  { Encoder::interrupt_common(8);  }
+    static void interrupt_enc_9()  { Encoder::interrupt_common(9);  }
+    static void interrupt_enc_10() { Encoder::interrupt_common(10); }
+    static void interrupt_enc_11() { Encoder::interrupt_common(11); }
+    static void interrupt_enc_12() { Encoder::interrupt_common(12); }
+    static void interrupt_enc_13() { Encoder::interrupt_common(13); }
+    static void interrupt_enc_14() { Encoder::interrupt_common(14); }
+    static void interrupt_enc_15() { Encoder::interrupt_common(15); }
+    static void interrupt_enc_16() { Encoder::interrupt_common(16); }
 };
-
-Encoders encoders;
