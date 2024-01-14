afacf5bfe78999fee3a68df100a26ccc97e6b0aa
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2024-01-14 17:10:46 +0300

    add SAVE_MODE

--- src/arduino_encoder.h
+++ src/arduino_encoder.h
@@ -32,10 +32,15 @@
 
 #define ENC_SETUP_IF_FOR_INTERRUPT(enc_number_test,pin_a,pin_b,fun_1,fun_2) if (Encoder::enc_number==enc_number_test) { \
   ENC_SETUP_INTERRUPT(pin_a,fun_1); if (pin_b!=ENC_NONE) ENC_SETUP_INTERRUPT(pin_b,fun_2); }
+#define OPERATION_ENC enc_dat__NOT_USING[enc_number]
 
 int enc_counter__NOT_USING = 0;
 bool enc_dat__NOT_USING[MAX_QUANTITY_ENC][6] = {0}; // [enc_number] [a_new,a_last,b_new,b_last,reverse_enc,one_pin_enc_flag]
 long int enc_count__NOT_USING[MAX_QUANTITY_ENC] = {0};
+
+#ifdef ENC_SAVE_MODE
+int enc_pin__NOT_USING[MAX_QUANTITY_ENC][2] = {0}; // [enc_number] [a_sensor_pin,a_sensor_pin]
+#endif
 
 class Encoder {
   public:
@@ -55,6 +60,10 @@ class Encoder {
       enc_dat__NOT_USING[Encoder::enc_number][4] = 0;
       enc_dat__NOT_USING[Encoder::enc_number][5] = (pin_b==ENC_NONE);
       enc_count__NOT_USING[Encoder::enc_number] = 0;
+      #ifdef ENC_SAVE_MODE
+      enc_pin__NOT_USING[Encoder::enc_number][0] = pin_a;
+      enc_pin__NOT_USING[Encoder::enc_number][1] = pin_b;
+      #endif
       ENC_SETUP_IF_FOR_INTERRUPT(0,pin_a,pin_b,Encoder::interrupt_enc_1,Encoder::interrupt_enc_2);
       ENC_SETUP_IF_FOR_INTERRUPT(1,pin_a,pin_b,Encoder::interrupt_enc_3,Encoder::interrupt_enc_4);
       ENC_SETUP_IF_FOR_INTERRUPT(2,pin_a,pin_b,Encoder::interrupt_enc_5,Encoder::interrupt_enc_6);
@@ -80,6 +89,27 @@ class Encoder {
   private:
     int enc_number;
     
+    #ifdef ENC_SAVE_MODE
+    static void interrupt_common(int interrupt_number) {
+      int enc_number = (interrupt_number-1)/2; // [enc_number] [a_new,a_last,b_new,b_last,reverse_enc,one_pin_enc_flag]
+      // read
+      OPERATION_ENC[0] = digitalRead(enc_pin__NOT_USING[enc_number][0]);
+      OPERATION_ENC[2] = digitalRead(enc_pin__NOT_USING[enc_number][1]);
+      // forward
+      if      (!OPERATION_ENC[1] &&  OPERATION_ENC[0] && !OPERATION_ENC[3] && !OPERATION_ENC[2]) enc_count__NOT_USING[enc_number] += int(OPERATION_ENC[4])*2-1;
+      else if ( OPERATION_ENC[1] &&  OPERATION_ENC[0] && !OPERATION_ENC[3] &&  OPERATION_ENC[2]) enc_count__NOT_USING[enc_number] += int(OPERATION_ENC[4])*2-1;
+      else if ( OPERATION_ENC[1] && !OPERATION_ENC[0] &&  OPERATION_ENC[3] &&  OPERATION_ENC[2]) enc_count__NOT_USING[enc_number] += int(OPERATION_ENC[4])*2-1;
+      else if (!OPERATION_ENC[1] && !OPERATION_ENC[0] &&  OPERATION_ENC[3] && !OPERATION_ENC[2]) enc_count__NOT_USING[enc_number] += int(OPERATION_ENC[4])*2-1;
+      // backward
+      if      (!OPERATION_ENC[1] &&  OPERATION_ENC[0] && !OPERATION_ENC[3] && !OPERATION_ENC[2]) enc_count__NOT_USING[enc_number] -= int(OPERATION_ENC[4])*2-1;
+      else if ( OPERATION_ENC[1] &&  OPERATION_ENC[0] && !OPERATION_ENC[3] &&  OPERATION_ENC[2]) enc_count__NOT_USING[enc_number] -= int(OPERATION_ENC[4])*2-1;
+      else if ( OPERATION_ENC[1] && !OPERATION_ENC[0] &&  OPERATION_ENC[3] &&  OPERATION_ENC[2]) enc_count__NOT_USING[enc_number] -= int(OPERATION_ENC[4])*2-1;
+      else if (!OPERATION_ENC[1] && !OPERATION_ENC[0] &&  OPERATION_ENC[3] && !OPERATION_ENC[2]) enc_count__NOT_USING[enc_number] -= int(OPERATION_ENC[4])*2-1;
+      // save
+      OPERATION_ENC[1] = OPERATION_ENC[0];
+      OPERATION_ENC[3] = OPERATION_ENC[2];
+    }
+    #else
     static void interrupt_common(int interrupt_number) {
       /* shematic:
        * // update data
@@ -92,12 +122,13 @@ class Encoder {
        * enc_dat_B_last = enc_dat_B_real;
        */
       int enc_number = (interrupt_number-1)/2;
-      enc_dat__NOT_USING[enc_number][0] = enc_dat__NOT_USING[enc_number][0]!=(interrupt_number%2);
-      enc_dat__NOT_USING[enc_number][2] = enc_dat__NOT_USING[enc_number][2]==(interrupt_number%2);
-      enc_count__NOT_USING[enc_number] += int(((enc_dat__NOT_USING[enc_number][0]!=enc_dat__NOT_USING[enc_number][3])||enc_dat__NOT_USING[enc_number][5])!=enc_dat__NOT_USING[enc_number][4])*2-1;
-      enc_dat__NOT_USING[enc_number][1] = enc_dat__NOT_USING[enc_number][0];
-      enc_dat__NOT_USING[enc_number][3] = enc_dat__NOT_USING[enc_number][2];
+      OPERATION_ENC[0] = OPERATION_ENC[0]!=(interrupt_number%2);
+      OPERATION_ENC[2] = OPERATION_ENC[2]==(interrupt_number%2);
+      enc_count__NOT_USING[enc_number] += int(((OPERATION_ENC[0]!=OPERATION_ENC[3])||OPERATION_ENC[5])!=OPERATION_ENC[4])*2-1;
+      OPERATION_ENC[1] = OPERATION_ENC[0];
+      OPERATION_ENC[3] = OPERATION_ENC[2];
     }
+    #endif
     
     static void interrupt_enc_1()  { Encoder::interrupt_common(1);  }
     static void interrupt_enc_2()  { Encoder::interrupt_common(2);  }
